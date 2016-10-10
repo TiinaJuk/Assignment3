51297fd23bbfb0be77feb7c741d84629e09ecfa6
TiinaJuk <gustiiju@student.gu.se>
2016-10-10 11:29:14 +0300

    Add files via upload

---
+++ CircuitMain.java
@@ -0,0 +1,31 @@
+import static dit948.SimpleIO.*;
+import static dit948.Random.*;
+
+/**
+ * This is the main class. It first creates two circuits, checks their
+ * equivalence and prints the result to the console. It also prompts the user to
+ * enter the number of random circuits to create (say n). It then creates n
+ * random circuits of maximal depth 5, and prints the circuits and their truth
+ * values to the console.
+ * 
+ * @author musard
+ * 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Use the set of variables "v", "w", "x", "y", "z", "a", "b"  
+		// for all circuits you create
+
+		// Use an assignment where  the true variables are "v", "y", "a"
+		
+		// Create two circuits for !(a /\ b) and !(a) \/ !(b) and check whether or
+		// not they are equivalent
+
+		// Print out n random circuits and their truth values using the 
+		// assignment above (for a user-provided input n).
+
+	}
+}
---
+++ Variable.java
@@ -0,0 +1,50 @@
+/**
+ * This is a subclass of Circuit implementing Variable in a Circuit
+ * 
+ */
+
+public class Variable extends Circuit {
+
+	// Private instance variable,
+	// name, a String representing a variable
+
+	// code here
+
+	// Constructor with parameters
+
+	public Variable(String name) {
+		// code here
+	}
+
+	/**
+	 * String representation of a variable of a circuit
+	 */
+
+	public String toString() {
+		// code here
+	}
+
+	/**
+	 * Returns the name of the variable
+	 */
+
+	public String getName() {
+		// code here
+	}
+	
+	/**
+	 * Returns true if the variable is true in the assignment
+	 */
+
+	public boolean isTrueIn(Assignment assignment) {
+		// code here
+	}
+
+	/**
+	 * Returns the array of free Variables in this Variable. 
+	 */
+
+	public Variable[] freeVariables() {
+		// code here
+	}
+}
